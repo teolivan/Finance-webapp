0cee83b983bddcc51add93ebcc11682c8a5ebb5a
olivia <oliviateoliva@gmail.com>
2025-04-13 22:39:52 +0200

    change port number

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_origins=["http://localhost:5173"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

4df36d49df1f9c1ecaf19b90534afb149c1cb9aa
olivia <oliviateoliva@gmail.com>
2025-04-23 21:37:42 +0200

    add navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from "react"
 import axios from 'axios'; //axios parses JSON automatically, makes code cleaner
+import Navbar from "./components/navbar";
 
 export default function Home() {
   const [ticker, setTicker] = useState("");
@@ -39,9 +40,10 @@ export default function Home() {
 
 
   return (
+    <div>
+      <Navbar/>
 
     <div className="flex flex-col justify-center items-center h-screen bg-gray-900">
-
       <img src="logo.svg" alt="" className="h-[200px] mb-8"/>
 
       <div className="flex items-center gap-x-4">
@@ -62,16 +64,17 @@ export default function Home() {
       {result && (
             <div className="flex flex-col justify-center items-center h-250 bg-gray-900 w-300 text-center">
               <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white ">{result.results.name} ({result.results.ticker})</h1> 
+              <img 
+                  src={`http://localhost:8000/branding-image?url=${encodeURIComponent(result.results.branding.icon_url)}`} 
+                  alt={`${result.results.ticker} icon`} 
+                  className="h-[100px] mb-4"
+                />  
               <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Locale: {result.results.locale}</p>
               <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Market: {result.results.market}</p>
-              <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">{result.results.description}</p>
-              <img 
-  src={`http://localhost:8000/branding-image?url=${encodeURIComponent(result.results.branding.icon_url)}`} 
-  alt={`${result.results.ticker} icon`} 
-  className="h-[100px] mb-4"
-/>             </div>
-
+              <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">{result.results.description}</p>           
+      </div>
         )}
+    </div>
     </div>
   );
 }

ef3b7cc50574aaf6f32269e56c641b62cbdd9845
olivia <oliviateoliva@gmail.com>
2025-04-23 13:41:01 +0200

    fix first API request

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,26 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:3000"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,29 @@
 'use client'
 import { useState } from "react"
+import axios from 'axios'; //axios parses JSON automatically, makes code cleaner
 
 export default function Home() {
   const [ticker, setTicker] = useState("");
+  const [result, setResult] = useState<StockResult | null>(null);
+
+  type StockResult = {
+    results: {
+      ticker: string;
+      name: string;
+      market: string;
+      price: string;
+    };
+  };
+
+  const stockSearch = async () => {
+    try {
+        const response = await axios.get<StockResult>(`http://localhost:8000/stock/${ticker}`);
+        setResult(response.data);
+
+    } catch (error) {
+      console.error("API error: ", error);
+    }
+  }
 
   return (
 
@@ -16,14 +37,22 @@ export default function Home() {
         <input
           type="text"
           value={ticker}
-          onInput={(e) => setTicker(e.currentTarget.value.toUpperCase())}
+          onChange={(e) => setTicker(e.target.value.toUpperCase())}
           className="w-[200px] h-[50px] bg-white text-gray-700 px-4 rounded focus:outline-none"
           placeholder="Enter ticker here..."
         />
-        <button className="bg-white hover:bg-gray-100 text-gray-700 h-[50px] font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+        <button onClick={stockSearch} className="bg-white hover:bg-gray-100 text-gray-700 h-[50px] font-semibold py-2 px-4 border border-gray-400 rounded shadow">
           Search
         </button>
       </div>
+
+      {result && (
+            <div>
+              <h2>Ticker: {result.ticker}</h2> //need to fix this
+              <p>Price: {result.price}</p>
+            </div>
+
+        )}
     </div>
   );
 }

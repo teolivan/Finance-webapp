d418e9c518bf875b80b057357ae68360d053bd4d
olivia <oliviateoliva@gmail.com>
2025-04-23 15:02:31 +0200

    not functioning

--- backend/main.py
+++ backend/main.py
@@ -49,6 +49,29 @@ async def get_stock_info(ticker: str):
         print(f"Error: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
     
+# to retrieve the logo & icon of the company
+@app.get("/branding-image")
+async def get_stock_info(url: str):
+    url = f"{url}"
+    print('test')
+    print(url)
+    params = {"apiKey": polygon_key}
+
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url, params=params)
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail="Polygon API error")
+
+        data = response.json()
+        return data
+
+    except Exception as e:
+        print(f"Error: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+
     # Retrieve aggregated historical OHLC (Open, High, Low, Close) and volume data for a specified stock ticker over a custom date range and time interval in Eastern Time (ET)
     # e.g. of request url http://localhost:8000/stock/OHLC/AAPL/1/day/2024-03-01/2024-03-10
 @app.get("/stock/OHLC/{ticker}/{multiplier}/{timespan}/{timefrom}/{timeto}") 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ import axios from 'axios'; //axios parses JSON automatically, makes code cleaner
 export default function Home() {
   const [ticker, setTicker] = useState("");
   const [result, setResult] = useState<StockResult | null>(null);
+  const [brandresult, setBrandResult] = useState<Branding | null>(null);
 
   type StockResult = {
     results: {
@@ -14,15 +15,39 @@ export default function Home() {
       price: string;
       description: string;
       locale: string;
-      logo_url: string;
+      branding: Branding; 
     };
+    
   };
+
+  type Branding = {
+    icon_url: string;
+    logo_url: string;
+  }
+
+  const handleClick = async () => {
+   const responseData = await stockSearch();
+   if(responseData === undefined) {
+      return;
+   }
+   brandImage(responseData.results.branding);
+  }
 
   const stockSearch = async () => {
     try {
         const response = await axios.get<StockResult>(`http://localhost:8000/stock/${ticker}`);
         setResult(response.data);
+        return response.data;
+    } catch (error) {
+      console.error("API error: ", error);
+    }
+    
+  }
 
+  const brandImage = async (responseData : Branding) => {
+    try {
+      const response = await axios.get<Branding>(`http://localhost:8000/branding-image?url=`)
+      setBrandResult(response.data);
     } catch (error) {
       console.error("API error: ", error);
     }
@@ -44,18 +69,18 @@ export default function Home() {
           className="w-[200px] h-[50px] bg-white text-gray-700 px-4 rounded focus:outline-none"
           placeholder="Enter ticker here..."
         />
-        <button onClick={stockSearch} className="bg-white hover:bg-gray-100 text-gray-700 h-[50px] font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+        <button onClick={handleClick} className="bg-white hover:bg-gray-100 text-gray-700 h-[50px] font-semibold py-2 px-4 border border-gray-400 rounded shadow">
           Search
         </button>
       </div>
 
       {result && (
-            <div className="flex flex-col justify-center items-center h-250 bg-gray-900 w-300">
-              <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">{result.results.name} ({result.results.ticker})</h1> 
+            <div className="flex flex-col justify-center items-center h-250 bg-gray-900 w-300 text-center">
+              <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white ">{result.results.name} ({result.results.ticker})</h1> 
               <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Locale: {result.results.locale}</p>
               <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Market: {result.results.market}</p>
               <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">{result.results.description}</p>
-              <img src={result.results.logo_url} alt={result.results.name} />
+              <img src={brandresult?.icon_url} alt={brandresult?.icon_url} className="h-[200px] mb-8"/>
             </div>
 
         )}
